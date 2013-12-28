daa85e7fb090624d343cd9f59395f37b2d583c24
Grzegorz Gajoch <gajoch@gmail.com>
2013-12-28 02:07:27 +0100

    First Commit

---
+++ MicroSat/MicroSat.cpp
@@ -0,0 +1,17 @@
+/*
+ * MicroSat.cpp
+ *
+ * Created: 2013-12-28 01:26:11
+ *  Author: rexina
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
